7d5d35998324ea4be20589accacc011d60fd40b5
ewan <ewan.friend@hotmail.com>
2026-08-12 01:08:01 +0100

    added font and wipe function

--- chip8_core/src/lib.rs
+++ chip8_core/src/lib.rs
@@ -7,12 +7,34 @@ const STACK_SIZE: usize = 16;
 const NUM_KEYS: usize = 16;
 const START_ADDRESS: u16 = 0x200;
 
+const FONTSET_SIZE: usize = 80;
+
+const FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, //0
+    0x20, 0x60, 0x20, 0x20, 0x70, //1
+    0xF0, 0x10, 0xF0, 0x80, 0xF0, //2
+    0xF0, 0x10, 0xF0, 0x10, 0xF0, //3
+    0x90, 0x90, 0xF0, 0x10, 0x10, //4
+    0xF0, 0x80, 0xF0, 0x10, 0xF0, //5
+    0xF0, 0x80, 0xF0, 0x90, 0xF0, //6
+    0xF0, 0x10, 0x20, 0x40, 0x40, //7
+    0xF0, 0x90, 0xF0, 0x90, 0xF0, //8
+    0xF0, 0x90, 0xF0, 0x10, 0xF0, //9
+    0xF0, 0x90, 0xF0, 0x90, 0x90, //A
+    0xE0, 0x90, 0xE0, 0x90, 0xE0, //B
+    0xF0, 0x80, 0x80, 0x80, 0xF0, //C
+    0xE0, 0x90, 0x90, 0x90, 0xE0, //D
+    0xF0, 0x80, 0xF0, 0x80, 0xF0, //E
+    0xF0, 0x80, 0xF0, 0x80, 0x80  //F
+];
+
 pub struct Chip8 {
     pc: u16,
     memory: [u8; MEM_SIZE],
     frame_buffer: [bool; SCREEN_HEIGHT * SCREEN_WIDTH],
     v_reg: [u8; NUM_REGS],
     i_reg: u16,
+    sp: u16,
     stack: [u16; STACK_SIZE],
     keys: [u16; NUM_KEYS],
     dt: u8,
@@ -21,16 +43,45 @@ pub struct Chip8 {
 
 impl Chip8 {
     pub fn new() -> Self {
-        Self {
+        let mut chip8_default = Self {
             pc: START_ADDRESS,
             memory: [0; MEM_SIZE],
             frame_buffer: [false; SCREEN_HEIGHT * SCREEN_WIDTH],
             v_reg: [0; NUM_REGS],
             i_reg: 0,
+            sp: 0,
             stack: [0; STACK_SIZE],
             keys: [0; NUM_KEYS],
             dt: 0,
             st: 0,
-        }
+        };
+
+        chip8_default.memory[0..FONTSET_SIZE].copy_from_slice(&FONTSET);
+
+        chip8_default
+    }
+
+    pub fn wipe(&mut self) {
+        self.pc = START_ADDRESS;
+        self.memory = [0; MEM_SIZE];
+        self.frame_buffer = [false; SCREEN_HEIGHT * SCREEN_WIDTH];
+        self.v_reg = [0; NUM_REGS];
+        self.i_reg = 0;
+        self.sp = 0;
+        self.stack = [0; STACK_SIZE];
+        self.keys = [0; NUM_KEYS];
+        self.dt = 0;
+        self.st = 0;
+        self.memory[0..FONTSET_SIZE].copy_from_slice(&FONTSET);
+    }
+
+    pub fn push(&mut self , val: u16) {
+        self.stack[self.sp as usize] = val;
+        self.sp += 1;
+    }
+
+    pub fn pop(&mut self) -> u16 {
+        self.sp -= 1;
+        self.stack[self.sp as usize]
     }
 }
