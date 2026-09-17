98185ef1c548353bf582fbef29b94d7434229528
ewan <ewan.friend@hotmail.com>
2026-09-17 18:03:23 +0100

    game running! (too fast)

--- desktop/src/main.rs
+++ desktop/src/main.rs
@@ -1,10 +1,18 @@
 use chip8_core::*;
 use std::env;
+use std::fs::File;
+use std::io::Read;
 use sdl2::event::Event;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+use sdl2::render::Canvas;
+use sdl2::video::Window;
 
 const SCALE: u32 = 15;
 const WINDOW_WIDTH: u32 = (SCREEN_WIDTH as u32) * SCALE;
 const WINDOW_HEIGHT: u32 = (SCREEN_HEIGHT as u32) * SCALE;
+
+const TICKS_PER_FRAME: usize = 1;
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -12,6 +20,7 @@ fn main() {
         println!("cargo run path/to/your/ROM");
         return;
     }
+    let filename = &args[1];
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
@@ -23,13 +32,16 @@ fn main() {
         .unwrap();
 
     let mut canvas = window.into_canvas().build().unwrap();
-    canvas.clear();
-    canvas.present();
+//    canvas.clear();
+//    canvas.present();
 
-    let mut chip8 = Emu::new();
+    let mut chip8 = Chip8::new();
+    let mut rom = File::open(filename).expect("Something went wrong when opening the file");
+    let mut buffer: Vec<u8> = Vec::new();
+    rom.read_to_end(&mut buffer).unwrap();
+    chip8.load(&buffer);
 
     let mut event_pump = sdl_context.event_pump().unwrap();
-
     'gameloop: loop {
         for e in event_pump.poll_iter() {
             match e {
@@ -39,5 +51,31 @@ fn main() {
                 _ => ()
             }
         }
+
+        for _ in 0..TICKS_PER_FRAME {
+            chip8.tick();
+        }
+        chip8.timer_tick();
+        draw(&chip8, &mut canvas);
     }
+}
+
+fn draw(emu: &Chip8, canvas: &mut Canvas<Window>) {
+    canvas.set_draw_color(Color::RGB(0, 0, 0));
+    canvas.clear();
+
+    let buffer = emu.get_display();
+
+    canvas.set_draw_color(Color::RGB(255, 255, 255));
+
+    for (i, pixel) in buffer.iter().enumerate() {
+        if *pixel {
+            let x = (i % SCREEN_WIDTH) as u32;
+            let y = (i / SCREEN_WIDTH) as u32;
+
+            let r = Rect::new((x * SCALE) as i32, (y * SCALE) as i32, SCALE, SCALE);
+            canvas.fill_rect(r).unwrap();
+        }
+    }
+    canvas.present();
 }
